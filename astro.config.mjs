63d9ed987fab5db5174ef7f2ba598b995c77dac4
Riley Paul <rileypaul96@gmail.com>
2026-03-05 16:47:02 -0700

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -10,4 +12,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
